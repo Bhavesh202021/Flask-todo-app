d7bef972fab6ec5f61777113177986d9bc94716d
Bhavesh202021 <sondagarbhavesh20@gmail.com>
2022-02-23 20:04:12 +0530

    Update about

--- app.py
+++ app.py
@@ -35,8 +35,7 @@ def hello_world():
     #return "<p>Hello, World!</p>"
 @app.route("/aboutus")
 def aboutus():
-    
-    return render_template('aboutus.html' , allTodo = allTodo) 
+    return render_template('aboutus.html') 
     
     
 @app.route("/show")
