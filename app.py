e2c897fecbe1dcc518a86582360d176dfbbc367e
bhavesh_234 <bhau87118@gmail.com>
2022-09-22 18:33:18 +0530

    updated file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask , render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+from ddtrace import tracer
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
@@ -18,11 +18,12 @@ class Todo(db.Model):
         return f'{self.sno} - {self.title}'
 
 
-
+@tracer.wrap("flask.request",service='flask-todo',resource='GET/POST',span_type='web')
 @app.route("/login/myto-do/feedback",methods=['GET','POST'])
 def feed_back():
     return render_template('feedback.html')
 
+@tracer.wrap("flask.request",service='flask-home',resource='GET/POST',span_type='web')
 @app.route("/login/myto-do",methods=['GET','POST'])
 def hello_world():
     if request.method == 'POST':
@@ -38,27 +39,30 @@ def hello_world():
     #print(allTodo)
     #return "<p>Hello, World!</p>"
 
-
+@tracer.wrap("flask.request",service='flask-login',resource='GET',span_type='web')
 @app.route("/login")
 def login():
     return render_template('login.html')
 
-
+@tracer.wrap("flask.request",service='flask-home',resource='GET',span_type='web')
 @app.route("/")
 def login1():
     return render_template('login.html')
-   
+
+@tracer.wrap("flask.request",service='flask-show',resource='GET',span_type='web')
 @app.route("/login/myto-do/show")
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
     return redirect("https://www.hotelmanagement.net/")
 
+
+@tracer.wrap("flask.request",service='flask-aboutus',resource='GET',span_type='web')
 @app.route("/login/myto-do/aboutus")
 def aboutus():
     return render_template('aboutus.html') 
 
-
+@tracer.wrap("flask.request",service='flask-todoupdate',resource='GET',span_type='web')
 @app.route("/login/myto-do/delete/<int:sno>")
 def delete(sno):
     todo = Todo.query.filter_by(sno =sno).first()
@@ -68,6 +72,8 @@ def delete(sno):
     #return "<p>This page for products page </p>"
     return redirect("/login/myto-do")
 
+
+@tracer.wrap("flask.request",service='flak_update',resource='GET/POST',span_type='web')
 @app.route("/login/myto-do/update/<int:sno>", methods=['GET','POST'])
 def update(sno):
     if request.method == 'POST':
